0631bf5eb21e377928385f572a39a1dc74fe431d
JordanPallone@QuickenLoans.com <jordanpallone@quickenloans.com>
2021-05-13 14:39:44 -0400

    first commit

---
+++ Lab11.3/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Lab11._3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab11._3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterResponse(string firstname, string lastname, string email, string pnum, string password, string confirmpassword)
+        {
+            if (password != confirmpassword)
+            {
+                return RedirectToAction("Register");
+            }
+
+            ViewData["FN"] = firstname;
+            ViewData["LN"] = lastname;
+            ViewData["EM"] = email;
+            ViewData["PN"] = pnum;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
